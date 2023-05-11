b7dca43929be7abffe90a5ed94a00457445adcd6
You JaeWon <jaewon000830@gmail.com>
2023-05-11 18:06:53 +0900

    2023-05-11 Backup DB에서 카테고리 받아와서 물품등록 관리자화면에 표시

--- SpringWeb/src/main/java/com/my/myapp/AdminController.java
+++ SpringWeb/src/main/java/com/my/myapp/AdminController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.shop.model.CategoryVO;
 import com.shop.model.ProductVO;
 import com.shop.service.AdminService;
 
@@ -33,6 +34,10 @@ public class AdminController {
 	
 	@GetMapping("/prodForm")
 	public String productForm(Model m) {
+		
+		//상위 카테고리 목록 가져오기
+		List<CategoryVO> upCgList=adminService.getUpcategory();
+		m.addAttribute("upCgList", upCgList);
 		
 		return "admin/prodInput";
 		//WEB-INF/views/admin/prodInput.jsp
