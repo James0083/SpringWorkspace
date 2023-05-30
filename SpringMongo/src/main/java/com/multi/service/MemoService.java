a694d41126cc81eb96972b5e911a69976ad511d7
You JaeWon <jaewon000830@gmail.com>
2023-05-30 21:16:13 +0900

    2023-05-30 Backup MemoApp MongoDB로 바꾸기 - input.jsp 에서 MongoDB로 데이터 추가

---
+++ SpringMongo/src/main/java/com/multi/service/MemoService.java
@@ -0,0 +1,14 @@
+package com.multi.service;
+
+import java.util.List;
+
+import com.multi.domain.MemoVO;
+
+public interface MemoService {
+
+	int insertMemo(MemoVO momo);
+	List<MemoVO> listMemo();
+	int deleteMemo(String id);
+	int updateMemo(MemoVO memo);
+	
+}
